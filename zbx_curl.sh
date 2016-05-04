0ec4100d1f4b9becc177387af8b3e863ca18d258
zphantomz <zphantomz@users.noreply.github.com>
2016-05-04 16:19:45 +0200

    Fixed compatibility with HP switch and cache usage

---
+++ zbx_curl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/usr/bin/curl -s $1
+echo 
+
--- zbxsnmputils.py
+++ zbxsnmputils.py
@@ -1,8 +1,9 @@
 import asyncio
 import json
+import time
+from collections import defaultdict
 from aiohttp import web
 from pysnmp.hlapi.asyncio import *
-
 from zbxsender import zbxsender
 
 import pprint
@@ -19,8 +20,10 @@ RES_CACHE = {}
 RES_CACHE_TIMEOUT = 3600
 @asyncio.coroutine
 def asnmp_query(svarbind, host="127.0.0.1", community="public", use_cache=True):
-    if "{}-{}".format(svarbind,host) in RES_CACHE.keys() and use_cache:
-        return RES_CACHE["{}-{}".format(svarbind,host)]
+    # Use cached query if use_cache is True and the cache is newer than RES_CACHE_TIMEOUT
+    if "{}-{}".format(svarbind, host) in RES_CACHE.keys() and use_cache:
+        if RES_CACHE["{}-{}".format(svarbind, host)][1] > int(time.time() - RES_CACHE_TIMEOUT):
+            return RES_CACHE["{}-{}".format(svarbind, host)][0]
     snmpEngine = SnmpEngine()
     c = True
     snmpdata = dict() 
@@ -45,7 +48,7 @@ def asnmp_query(svarbind, host="127.0.0.1", community="public", use_cache=True):
                 errorStatus.prettyPrint(),
                 errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
         else:
-            # uso il reqoid per capire quando sono uscito dalla table snmp cambiando bloccando
+            # uso il reqoid per capire quando sono uscito dalla table snmp bloccando
             # il ciclo while
             for varBindRow in varBindTable:
                 reqoid = tuple(startoid[0])
@@ -55,18 +58,13 @@ def asnmp_query(svarbind, host="127.0.0.1", community="public", use_cache=True):
                     #print("Oid chiesto   ", reqoid)
                     #print("Oid analizzato", getoid)
                     #print("Indice:       ",getoid[len(reqoid):])
-                    #print("Analisi:       ",getoid[:len(reqoid)])
-                    #print('oid: {} - idx:{} - value: {} '.format(varBindRow[0][0].prettyPrint(),
-                    #                                             getoid[len(reqoid):][0],
-                    #                                             varBindRow[0][1].prettyPrint(),
-                    #                                             ))
                 else:
                     c = False
                     break
         varBinds = varBindTable[-1][0]
 
     snmpEngine.transportDispatcher.closeDispatcher()
-    RES_CACHE["{}-{}".format(svarbind,host)] = snmpdata
+    RES_CACHE["{}-{}".format(svarbind,host)] = (snmpdata, int(time.time()))
     return snmpdata
 
 @asyncio.coroutine
@@ -79,35 +77,40 @@ def trunkports_handle(request):
     """
     host = request.GET.get('host', '127.0.0.1')
     community = request.GET.get('community', 'public')
-    ifnameidx = yield from asnmp_query(OID['ifName'],
-                                      host, community)
-    iftypeidx = yield from asnmp_query(OID['ifType'],
-                                      host, community)
-    bridgeidx = yield from asnmp_query(OID['dot1dBasePortIfIndex'],
-                                      host, community)
-    bridgeidx = {k: int(v) for k,v in bridgeidx.items()}
-    snmpdata = [{'ifIndex': v, 'ifName': ifnameidx[v]} for k, v in bridgeidx.items()
+    asnmp_functions = [asnmp_query(OID['ifName'], host, community),
+                       asnmp_query(OID['ifType'], host, community),
+                       asnmp_query(OID['dot1dBasePortIfIndex'], host, community)
+                      ]
+    asnmp_functions_res = yield from asyncio.gather(*asnmp_functions)
+    ifnameidx = asnmp_functions_res[0]
+    iftypeidx = asnmp_functions_res[1]
+    bridgeidx = defaultdict(int)
+    for k,v in asnmp_functions_res[2].items():
+       bridgeidx[k] = int(v)
+    # bridgeidx = {k: int(v) for k,v in asnmp_functions_res[2].items()}
+
+    snmpdata = [{'#IFINDEX': v, '#IFNAME': ifnameidx[v]} for k, v in bridgeidx.items()
                  if not iftypeidx[v] == "6"]
+    # Zabbix LLD Format
+    snmpdata = {'data' : snmpdata}
     body = json.dumps(snmpdata).encode('utf8')
     content_type = "application/json"
     return web.Response(body=body, content_type=content_type)
 
 @asyncio.coroutine
 def staticvlans_handle(request):
-    """Retrieve Access and Tagged port for every vlan_static_port
-    NonStandard:
-    the oid_index is not correcly returned from easysnmp, need to calculated from base oid
+    """Retrieve Access and Tagged port for every static Vlan
 
     Return:
     Dict:
        vlanId1:
              name: 'vlan name'
              access_ports: ['ifName1', 'ifName2'...]
-             tagged_ports: ['ifName1', 'ifName2'...]
+             tagged_trunks: ['ifName1', 'ifName2'...]
        vlanId2:
              name: 'vlan name'
              access_ports: ['ifName1', 'ifName2'...]
-             tagged_ports: ['ifName1', 'ifName2'...]
+             tagged_trunks: ['ifName1', 'ifName2'...]
 
     How it works:
     the vlan table returned from dot1qVlanStatic oid contain the name, and the ports assigned to the vlan
@@ -141,22 +144,13 @@ def staticvlans_handle(request):
     vlans_name = asnmp_functions_res[0]
     vlans_egress_ports = asnmp_functions_res[1]
     vlans_untagged_ports = asnmp_functions_res[2]
-    bridgeIdx_to_ifIdx = {k: int(v) for k,v in asnmp_functions_res[3].items()}
+    bridgeIdx_to_ifIdx = defaultdict(int)
+    for k,v in asnmp_functions_res[3].items():
+       bridgeIdx_to_ifIdx[k] = int(v)
+    # bridgeIdx_to_ifIdx = {k: int(v) for k,v in asnmp_functions_res[3].items()}
     ifName = asnmp_functions_res[4]
     ifType = asnmp_functions_res[5]
-    """
-    vlans_name = yield from asnmp_query(OID['dot1qVlanStaticName'],
-                                        host, community)
-    vlans_egress_ports = yield from asnmp_query(OID['dot1qVlanStaticEgressPorts'],
-                                                host, community)    
-    vlans_untagged_ports = yield from asnmp_query(OID['dot1qVlanStaticUntaggedPorts'],
-                                                  host, community)  
-    bridgeIdx_to_ifIdx = yield from asnmp_query(OID['dot1dBasePortIfIndex'],
-                                                host, community)  
-    bridgeIdx_to_ifIdx = {k: int(v) for k,v in bridgeIdx_to_ifIdx.items()}
-    ifName = yield from asnmp_query(OID['ifName'], host, community)
-    ifType = yield from asnmp_query(OID['ifType'], host, community)
-    """
+
     for vlan_id, vlan in vlans_name.items():
         vlans_info[vlan_id] = dict()
         # And now the magic!, from a OctetString to a maps of ports
@@ -167,9 +161,9 @@ def staticvlans_handle(request):
             vlan_access_map += '{:08b}'.format(int(vlans_untagged_ports[vlan_id][i:i+2],16))
 
         vlan_access_port_list = [n + 1 for n, i in enumerate(vlan_access_map)
-                                 if i == '1' and n < len(bridgeIdx_to_ifIdx)]
+                                 if i == '1']
         vlan_tagged_port_list = [n + 1 for n, i in enumerate(vlan_egress_map)
-                                 if i == '1' and vlan_access_map[n] == '0' and n < len(bridgeIdx_to_ifIdx)]
+                                 if i == '1' and vlan_access_map[n] == '0']
         vlans_info[vlan_id]['name'] = vlan
 
         # Filtered on ifType=6 (ethernet) no trunks
